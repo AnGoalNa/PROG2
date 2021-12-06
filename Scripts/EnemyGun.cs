ff759ae9a09d0588b3af30a515ad71fb9bcbc140
AnGoalNa <72206900+AnGoalNa@users.noreply.github.com>
2021-12-06 16:02:03 +0100

    Add files via upload

---
+++ Scripts/EnemyGun.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour
+{
+    public GameObject EnemyBulletGO;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      Invoke ("FireEnemyBullet", 1f);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FireEnemyBullet()
+    {
+        GameObject playership = GameObject.Find ("PlayerGO");
+        if(playership != null){
+                GameObject bullet = (GameObject)Instantiate(EnemyBulletGO);
+                bullet.transform.position = transform.position;
+                Vector2 direction = playership.transform.position - bullet.transform.position;
+                bullet.GetComponent<EnemyBullet>().SetDirection(direction);
+        }
+
+    }
+}
---
+++ Scripts/PlayerControl.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerControl : MonoBehaviour 
+{
+	public GameObject GameManagerGO;
+	public GameObject PlayerBulletGO;
+	public GameObject bulletposition01;
+	public GameObject bulletposition02;
+	public GameObject ExplosionGO;
+	public Text LivesUIText;
+	const int MaxLives = 3;
+	int lives;
+
+
+	public float speed;
+
+	public void Init()
+	{
+		lives = MaxLives;
+		LivesUIText.text = lives.ToString ();
+		transform.position = new Vector2 (0, 0);
+		gameObject.SetActive (true);
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown("space"))
+		{
+			gameObject.GetComponent<AudioSource>().Play();
+			GameObject bullet01 = (GameObject)Instantiate (PlayerBulletGO);
+			bullet01.transform.position = bulletposition01.transform.position;
+
+			GameObject bullet02 = (GameObject)Instantiate (PlayerBulletGO);
+			bullet02.transform.position = bulletposition02.transform.position;
+
+		}
+		float x = Input.GetAxisRaw ("Horizontal");//the value will be -1, 0 or 1 (for left, no input, and right)
+		float y = Input.GetAxisRaw ("Vertical");//the value will be -1, 0 or 1 (for down, no input, and up)
+
+		//now based on the input we compute a direction vector, and we normalize it to get a unit vector
+		Vector2 direction = new Vector2 (x, y).normalized;
+
+		//noe we call the function that computes and sets the player's position
+		Move (direction);
+	}
+
+	void Move(Vector2 direction)
+	{
+		//find the screen limits to the player's movement (left, right, top and bottom edges of the screen)
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0)); //this is the bottom-left point (corner) of the screen
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1)); //this is the top-right point (corner) of the screen
+
+		max.x = max.x - 0.225f; //subtract the player sprite half width
+		min.x = min.x + 0.225f; //add the player sprite half width
+
+		max.y = max.y - 0.285f; //subtract the player sprite half height
+		min.y = min.y + 0.285f; //add the player sprite half height
+
+		//Get the player's current position
+		Vector2 pos = transform.position;
+
+		//Calculate the new position
+		pos += direction * speed * Time.deltaTime;
+
+		//Make sure the new position is outside the screen
+		pos.x = Mathf.Clamp (pos.x, min.x, max.x);
+		pos.y = Mathf.Clamp (pos.y, min.y, max.y);
+
+		//Update the player's position
+		transform.position = pos;
+	}
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if((col.tag == "EnemyShipTag") || (col.tag == "EnemyBulletTag"))
+		{
+			PlayExplosion();
+		lives--;
+		LivesUIText.text = lives.ToString();
+		if(lives == 0)
+		{
+				GameManagerGO.GetComponent<GameManager>().SetGameManagerState(GameManager.GameManagerState.GameOver);
+
+			gameObject.SetActive(false);
+		}
+		}
+	}
+	void PlayExplosion()
+	{
+		GameObject explosion = (GameObject)Instantiate (ExplosionGO);
+		explosion.transform.position = transform.position;
+	}
+
+}
